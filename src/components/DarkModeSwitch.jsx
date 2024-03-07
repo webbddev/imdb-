4114ace946f77b3eb626ec97eb7764b3112a5f90
Nikolaj <nikolaj.tetradov@gmail.com>
2024-03-07 12:48:44 +0200

    add Theme Toggle

---
+++ src/components/DarkModeSwitch.jsx
@@ -0,0 +1,53 @@
+'use client';
+
+import { MdLightMode, MdDarkMode } from 'react-icons/md';
+
+import { useTheme } from 'next-themes';
+export default function DarkModeSwitch() {
+  const { theme, setTheme, systemTheme } = useTheme();
+  const currentTheme = theme === 'system' ? systemTheme : theme;
+
+  const toggleDarkMode = () => {
+    if (theme === 'dark') {
+      setTheme('light');
+    } else {
+      setTheme('dark');
+    }
+  };
+
+  return (
+    <button onClick={toggleDarkMode}>
+      {currentTheme === 'dark' ? (
+        <MdLightMode className='text-xl cursor-pointer hover:text-amber-500' />
+      ) : (
+        <MdDarkMode className='text-xl cursor-pointer hover:text-amber-500' />
+      )}
+    </button>
+  );
+}
+
+// 'use client';
+
+// import { MdLightMode, MdDarkMode } from 'react-icons/md';
+
+// import { useTheme } from 'next-themes';
+// export default function DarkModeSwitch() {
+//   const { theme, setTheme, systemTheme } = useTheme();
+//   const currentTheme = theme === 'system' ? systemTheme : theme;
+
+//   return (
+//     <div>
+//       {currentTheme === 'dark' ? (
+//         <MdLightMode
+//           onClick={() => setTheme('light')}
+//           className='text-xl cursor-pointer hover:text-amber-500'
+//         />
+//       ) : (
+//         <MdDarkMode
+//           onClick={() => setTheme('dark')}
+//           className='text-xl cursor-pointer hover:text-amber-500'
+//         />
+//       )}
+//     </div>
+//   );
+// }

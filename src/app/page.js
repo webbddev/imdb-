17ea4652c6400ef2d2368342aed587dac4d8e59b
Nikolaj <nikolaj.tetradov@gmail.com>
2024-03-07 23:33:44 +0200

    error.jsx create

--- src/app/page.js
+++ src/app/page.js
@@ -5,36 +5,23 @@ const API_KEY = process.env.API_TMDB_KEY;
 export default async function Home({ searchParams }) {
   const genre = searchParams.genre || 'fetchTrending';
 
-  try {
-    const res = await fetch(
-      `https://api.themoviedb.org/3/${
-        genre === 'fetchTrending' ? `/movie/top_rated` : `/trending/all/week`
-      }?api_key=${API_KEY}&language=en-US&page=1`
-    );
+  const res = await fetch(
+    `https://api.themoviedb.org/3/${
+      genre === 'fetchTrending' ? `/movie/top_rated` : `/trending/all/week`
+    }?api_key=${API_KEY}&language=en-US&page=1`
+  );
 
-    if (!res.ok) {
-      throw new Error('Failed to fetch data');
-    }
+  const data = await res.json();
 
-    const data = await res.json();
-    const results = data.results;
-
-    return (
-      <div>
-        <Results results={results} />
-        {/* {results.map((result) => (
-          <div key={result.id}>
-            <h2>{result.original_title}</h2>
-          </div>
-        ))} */}
-      </div>
-    );
-  } catch (error) {
-    console.error('An error occurred:', error);
-    return (
-      <div>
-        <p>An error occurred while fetching data.</p>
-      </div>
-    );
+  if (!res.ok) {
+    throw new Error('Failed to fetch data');
   }
+
+  const results = data.results;
+
+  return (
+    <div>
+      <Results results={results} />
+    </div>
+  );
 }

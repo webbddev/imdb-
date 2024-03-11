72106f08ddeea0b0501f341fbf2475340d093f14
Nikolaj <nikolaj.tetradov@gmail.com>
2024-03-11 17:23:30 +0200

    add card component

---
+++ src/components/Card.jsx
@@ -0,0 +1,36 @@
+import Image from 'next/image';
+import Link from 'next/link';
+import { FiThumbsUp } from 'react-icons/fi';
+
+export default function Card({ result }) {
+  return (
+    <div className='group cursor-pointer sm:hover:shadow-teal-400 sm:shadow-md border sm:border-gray-200 sm:m-2 dark:border-gray-700 rounded-lg shadow-md hover:shadow-2xl transition-shadow duration-300'>
+      <Link href={`/movie/${result.id}`}>
+        <Image
+          src={`https://image.tmdb.org/t/p/original/${
+            result.backdrop_path || result.poster_path
+          }`}
+          alt={result.original_title}
+          width={500}
+          height={500}
+          className='sm:rounded-t-lg group-hover:opacity-80 transition-opacity duration-200'
+        />
+      </Link>
+      <div className='p-2'>
+        <h3 className='text-ld font-bold truncate'>
+          {result.title || result.original_name}
+        </h3>
+        <p className='line-clamp-2 text-sm md:text-md mb-2'>
+          {result.overview}
+        </p>
+        <p className='text-sm flex items-center'>
+          Released: {result.release_date || result.first_air_date}
+          <div className='flex text-amber-600 dark:text-amber-400'>
+            <FiThumbsUp className='h-5 mr-1 ml-3 ' />
+            {result.vote_count}
+          </div>
+        </p>
+      </div>
+    </div>
+  );
+}

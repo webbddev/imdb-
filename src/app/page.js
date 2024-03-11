d49842048ff7c786816edfa27e68159aa70fdafc
Nikolaj <nikolaj.tetradov@gmail.com>
2024-03-11 14:37:20 +0200

    add loading effect

--- src/app/page.js
+++ src/app/page.js
@@ -9,6 +9,7 @@ export default async function Home({ searchParams }) {
     `https://api.themoviedb.org/3/${
       genre === 'fetchTrending' ? `/movie/top_rated` : `/trending/all/week`
     }?api_key=${API_KEY}&language=en-US&page=1`
+    // { next: { revalidate: 10000 } }
   );
 
   const data = await res.json();

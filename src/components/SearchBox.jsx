d2561587593736872ed3b11ed4c4c6a664dc18d3
Nikolaj <nikolaj.tetradov@gmail.com>
2024-03-13 21:16:56 +0200

    suspense change introduced

--- src/components/SearchBox.jsx
+++ src/components/SearchBox.jsx
@@ -6,10 +6,12 @@ import { useRouter } from 'next/navigation';
 export default function SearchBox() {
   const [search, setSearch] = useState('');
   const router = useRouter();
+
   const handleSubmit = (e) => {
     e.preventDefault();
     router.push(`/search/${search}`);
   };
+
   return (
     <form
       className='flex justify-between px-5 max-w-6xl mx-auto'
@@ -17,7 +19,7 @@ export default function SearchBox() {
     >
       <input
         type='text'
-        placeholder='Search keywords...'
+        placeholder='Search a...'
         className='w-full h-14 rounded-md placeholder-gray-500 outline-none bg-transparent flex-1'
         value={search}
         onChange={(e) => setSearch(e.target.value)}
